4c95de761f46b9bb864081c0ad45e79fc550c9f0
Zubair Syed <zubair.syed@ten10.com>
2025-02-07 12:46:29 +0000

    Trying to get openlayer map working.

---
+++ src/main/client/vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.ts
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+    build: {
+      outDir: '../resources/',
+      assetsDir:'static',
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'templates/flight.html'),
+        },
+      },
+    },
+  });
